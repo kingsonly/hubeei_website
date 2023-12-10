9280d8519d032938e4b3ebf97479de2a871e8539
kingsley achumie <kingsonly13c@gmail.com>
2023-12-10 18:40:54 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["api.hubeei.skillzserver.com", "otherdomain.com"],
+  },
 };
 
 module.exports = nextConfig;
