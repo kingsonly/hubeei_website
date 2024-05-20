0b792dfe9f8a98c0e7da7404a89603a3b90805ba
Dubem Nwodo <127799784+DevManchi@users.noreply.github.com>
2024-05-20 23:19:33 +0400

    feat: Add Steps section

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './sections/Hero'
+import Steps from './sections/Steps'
 
 function Home() {
   return (
     <div className=' bg-white'>
     <Hero/>
-    Home
+    <Steps/>
+
     </div>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,10 +4,10 @@ import Navbar from '../components/Navbar/Navbar'
 
 const Hero = () => {
   return (
-    <section className='max-container  w-full h-screen relative bg-hero-bg bg-cover bg-no-repeat bg-center top-0' >
+    <section className='max-container  w-full h-screen relative bg-hero-bg bg-cover bg-no-repeat bg-center' >
     <Navbar/>
     <div className="h-full w-full absolute bg-black opacity-85"/>
-    Hero
+
 
     <div className='padding_container'>
     <div className="hero-content text-center">
